86123c191e52e5afcbcca95b3534e1785f35174d
MEOFIXBUG <57151978+MEOFIXBUG@users.noreply.github.com>
2019-11-07 23:22:07 +0700

    .

---
+++ ProjectBatchName/Model/System Object/StringArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBatchName.Model
+{
+    public interface IStringArgs
+    {
+    }
+    
+    class ReplaceArgs : IStringArgs
+    {
+        string from;
+        string to;
+
+        public string From { get => from; set => from = value; }
+        public string To { get => to; set => to = value; }
+    }
+
+    class NewCaseArgs: IStringArgs
+    {
+        int mode;
+
+        public int Mode { get => mode; set => mode = value; }
+    }
+
+    class MoveArgs: IStringArgs
+    {
+        int mode;
+
+        public int Mode { get => mode; set => mode = value; }
+    }
+
+ 
+}
